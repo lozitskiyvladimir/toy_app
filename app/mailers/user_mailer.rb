c481d58a05f65cf4cd3211b9e050d60ee63ae21e
Lozitskiy Vladimir <loza-w@mail.ru>
2016-09-27 14:19:10 +0300

    Change email text

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,10 +2,10 @@ class UserMailer < ApplicationMailer
   # default    :from => 'loza@rubyonrailsapp.tk'
   def message_1
     mail(
-      :subject => 'Hello from Postmark',
+      :subject => 'Hi,its me',
       :to  => 'loza-w@mail.ru',
       :from => 'loza@rubyonrailsapp.tk',
-      :html_body => '<strong>Hello</strong> dear Postmark user!!!.',
+      :html_body => '<strong>Hello</strong> Its me!!!.',
       :track_opens => 'true')
   end
 end
